a6832429c53047f19bd125b6cf4705c300d969f1
brightdragom <brightdragom@gmail.com>
2021-02-22 19:04:47 +0900

    Customer.js추가2

--- src/App.js
+++ src/App.js
@@ -4,20 +4,47 @@ import Customer from './components/Customer'
 
 function App() {
 
-  const customer = {
+  const customers = [{
+    'id':1,
+    'image':'https://placeimg.com/64/64/1',
     'name':'홍길동',
     'birthday':'970411',
     'gender':'남자',
     'job':'대학생'
-  }
+  },{
+    'id':2,
+    'image':'https://placeimg.com/64/64/2',
+    'name':'일지매',
+    'birthday':'0566068',
+    'gender':'남자',
+    'job':'대학생'
+  },{
+    'id':3,
+    'image':'https://placeimg.com/64/64/3',
+    'name':'이순신',
+    'birthday':'970411',
+    'gender':'남자',
+    'job':'대학생'
+  }]
 
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div>
+      {
+        customers.map(c => {
+          return(
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
+    </div>
   );
 }
 
